92459b23b03eeaa9e7333a5a226682611d3de1f5
Marivatis <martyniuk.tech@gmail.com>
2025-08-08 19:01:40 +0300

    feat(logger): add Logger interface and zap logger adapter

---
+++ internal/logger/zap_logger.go
@@ -0,0 +1,43 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type ZapLogger struct {
+	logger *zap.Logger
+}
+
+func NewZapLogger(cfg zap.Config) (*ZapLogger, error) {
+	zapLogger, err := cfg.Build()
+	if err != nil {
+		return nil, err
+	}
+	return &ZapLogger{logger: zapLogger}, nil
+}
+
+func (l *ZapLogger) Info(msg string, fields ...Field) {
+	zapFields := toZapFields(fields)
+	l.logger.Info(msg, zapFields...)
+}
+func (l *ZapLogger) Debug(msg string, fields ...Field) {
+	zapFields := toZapFields(fields)
+	l.logger.Debug(msg, zapFields...)
+}
+func (l *ZapLogger) Warn(msg string, fields ...Field) {
+	zapFields := toZapFields(fields)
+	l.logger.Warn(msg, zapFields...)
+}
+func (l *ZapLogger) Error(msg string, fields ...Field) {
+	zapFields := toZapFields(fields)
+	l.logger.Error(msg, zapFields...)
+}
+
+func toZapFields(fields []Field) []zapcore.Field {
+	zapFields := make([]zapcore.Field, 0, len(fields))
+	for _, f := range fields {
+		zapFields = append(zapFields, zap.Any(f.Key, f.Value))
+	}
+	return zapFields
+}
