c571ef6172c10dd1895f7a8ce54c0bf831396d2d
Marivatis <martyniuk.tech@gmail.com>
2025-08-09 13:37:54 +0300

    feat(postgres): add db connection function and update go.mod, go.sum dependencies

---
+++ internal/repository/postgres/postgres.go
@@ -0,0 +1,36 @@
+package postgres
+
+import (
+	"Personal-Notes/internal/config"
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"time"
+)
+
+func NewPostgresDB(ctx context.Context, cfg *config.Config) (*pgxpool.Pool, error) {
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBSSLMode,
+	)
+
+	cfgPool, err := pgxpool.ParseConfig(connStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse config: %w", err)
+	}
+
+	cfgPool.MaxConns = 25
+	cfgPool.MaxConnLifetime = time.Hour
+
+	pool, err := pgxpool.NewWithConfig(ctx, cfgPool)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create pgx pool: %w", err)
+	}
+
+	if err = pool.Ping(ctx); err != nil {
+		pool.Close()
+		return nil, fmt.Errorf("failed to pіng pgx pool: %w", err)
+	}
+
+	return pool, nil
+}
