cf0dcfb5d91af46a8b96f1e50b3bf3ba3104e2ee
Marivatis <martyniuk.tech@gmail.com>
2025-08-13 17:31:16 +0300

    feat(migrations): add created_at and updated_at to notes table schema

--- migrations/202508091512_create_notes_table.up.sql
+++ migrations/202508091512_create_notes_table.up.sql
@@ -3,6 +3,8 @@ CREATE TABLE notes (
     owner_id INT NOT NULL,
     title VARCHAR(255) NOT NULL,
     body TEXT,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
     CONSTRAINT fk_notes_owner_id
        FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
 );
