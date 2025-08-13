64abaafdd38cb7cab67cf713662ae0031a9cdcff
Marivatis <martyniuk.tech@gmail.com>
2025-08-13 18:22:04 +0300

    feat(repository): define Note repository interface

---
+++ internal/repository/repository.go
@@ -0,0 +1,17 @@
+package repository
+
+import (
+	"Personal-Notes/internal/entity"
+	"context"
+)
+
+type Note interface {
+	Create(ctx context.Context, note entity.Note) (entity.Note, error)
+	GetById(ctx context.Context, id int) (entity.Note, error)
+	Update(ctx context.Context, note entity.Note) (entity.Note, error)
+	Delete(ctx context.Context, id int) error
+}
+
+type Repository struct {
+	Note
+}
