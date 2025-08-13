e5bfa5364bfad80181cc083209e7a12cc55b9f53
Marivatis <martyniuk.tech@gmail.com>
2025-08-13 17:23:56 +0300

    fix(entity): export struct fields for User

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -3,11 +3,11 @@ package entity
 import "time"
 
 type User struct {
-	id          int
-	name        string
-	email       string
-	password    string
-	createdAt   time.Time
-	updatedAt   time.Time
-	lastLoginAt time.Time
+	ID          int
+	Name        string
+	Email       string
+	Password    string
+	CreatedAt   time.Time
+	UpdatedAt   *time.Time
+	LastLoginAt *time.Time
 }
