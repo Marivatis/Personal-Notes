9c0836ef63db1fcf1e79c999bd74fd8aa0f213d6
Marivatis <martyniuk.tech@gmail.com>
2025-08-14 15:59:18 +0300

    feat(postgres): add Note Create method implementation

--- internal/repository/postgres/note_repo.go
+++ internal/repository/postgres/note_repo.go
@@ -3,8 +3,12 @@ package postgres
 import (
 	"Personal-Notes/internal/entity"
 	"Personal-Notes/internal/logging"
+	"Personal-Notes/internal/repository"
 	"context"
+	"errors"
+	"fmt"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"time"
 )
 
 const (
@@ -44,7 +48,48 @@ func NewNoteRepository(db *pgxpool.Pool, logger logging.Logger) *NoteRepository 
 }
 
 func (r *NoteRepository) Create(ctx context.Context, note entity.Note) (entity.Note, error) {
-	return entity.Note{}, nil
+	start := time.Now()
+
+	note.CreatedAt = start
+
+	r.logger.Debug("monitor[note]: starting note db insertion",
+		logging.NewField("owner_id", note.OwnerID),
+		logging.NewField("title", note.Title),
+		logging.NewField("body", note.Body),
+		logging.NewField("created_at", note.CreatedAt),
+	)
+
+	var resp entity.Note
+
+	err := r.db.QueryRow(ctx, sqlCreateNote,
+		note.OwnerID, note.Title, note.Body, note.CreatedAt).
+		Scan(&resp.ID, &resp.OwnerID, &resp.Title, &resp.Body, &resp.CreatedAt, &resp.UpdatedAt)
+	if err != nil {
+		if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+			r.logger.Error(fmt.Sprintf("fail[note]: %e", repository.ErrTimeout),
+				logging.NewField("owner_id", note.OwnerID),
+				logging.NewField("title", note.Title),
+				logging.NewField("operation", "insert"),
+				logging.NewField("duration", time.Since(start)),
+				logging.NewField("error", err),
+			)
+			return entity.Note{}, fmt.Errorf("%w: %w", repository.ErrTimeout, err)
+		}
+		r.logger.Error(fmt.Sprintf("fail[note]: %e", repository.ErrDB),
+			logging.NewField("owner_id", note.OwnerID),
+			logging.NewField("title", note.Title),
+			logging.NewField("operation", "insert"),
+			logging.NewField("duration", time.Since(start)),
+		)
+		return entity.Note{}, fmt.Errorf("%w: %w", repository.ErrDB, err)
+	}
+
+	r.logger.Info("done[note]: inserted successfully",
+		logging.NewField("id", resp.ID),
+		logging.NewField("owner_id", resp.OwnerID),
+		logging.NewField("title", resp.Title),
+	)
+	return resp, nil
 }
 func (r *NoteRepository) GetById(ctx context.Context, id int) (entity.Note, error) {
 	return entity.Note{}, nil
