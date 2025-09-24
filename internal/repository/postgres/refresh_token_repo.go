afe8a6db43775a968bcc5c0865cf5ce108462ca3
Marivatis <martyniuk.tech@gmail.com>
2025-09-24 18:36:31 +0300

    feat(postgres): implement RefreshToken Create method

--- internal/repository/postgres/refresh_token_repo.go
+++ internal/repository/postgres/refresh_token_repo.go
@@ -39,3 +39,77 @@ const (
 		RETURNING id
 	`
 )
+
+type RefreshTokenRepository struct {
+	db     *pgxpool.Pool
+	logger logging.Logger
+}
+
+func NewRefreshTokenRepository(db *pgxpool.Pool, logger logging.Logger) *RefreshTokenRepository {
+	return &RefreshTokenRepository{
+		db:     db,
+		logger: logger,
+	}
+}
+
+func (r *RefreshTokenRepository) Create(
+	ctx context.Context,
+	refreshToken entity.RefreshToken,
+) (entity.RefreshToken, error) {
+	start := time.Now()
+
+	refreshToken.CreatedAt = start
+
+	r.logger.Debug("monitor[refresh_token]: starting refresh token db insertion",
+		logging.NewField("user_id", refreshToken.UserID),
+		logging.NewField("expires_at", refreshToken.ExpiresAt),
+		logging.NewField("created_at", refreshToken.CreatedAt),
+	)
+
+	var resp entity.RefreshToken
+
+	err := r.db.QueryRow(ctx, sqlCreateRefreshToken,
+		refreshToken.UserID, refreshToken.TokenHash, refreshToken.ExpiresAt, refreshToken.CreatedAt).
+		Scan(&resp.ID, &resp.UserID, &resp.TokenHash, &resp.ExpiresAt, &resp.CreatedAt, &resp.RevokedAt, &resp.ReplacedByToken)
+	if err != nil {
+		if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+			r.logger.Error(fmt.Sprintf("fail[refresh_token]: %v", repository.ErrTimeout),
+				logging.NewField("user_id", refreshToken.UserID),
+				logging.NewField("operation", "insert"),
+				logging.NewField("duration", time.Since(start)),
+				logging.NewField("error", err),
+			)
+			return entity.RefreshToken{}, fmt.Errorf("%w: %w", repository.ErrTimeout, err)
+		}
+		r.logger.Error(fmt.Sprintf("fail[refresh_token]: %v", repository.ErrDB),
+			logging.NewField("user_id", refreshToken.UserID),
+			logging.NewField("operation", "insert"),
+			logging.NewField("duration", time.Since(start)),
+		)
+		return entity.RefreshToken{}, fmt.Errorf("%w: %w", repository.ErrDB, err)
+	}
+
+	r.logger.Info("done[refresh_token]: inserted successfully",
+		logging.NewField("id", resp.ID),
+		logging.NewField("user_id", resp.UserID),
+	)
+	return resp, nil
+}
+
+func (r *RefreshTokenRepository) GetByToken(
+	ctx context.Context,
+	tokenHash string,
+) (entity.RefreshToken, error) {
+}
+
+func (r *RefreshTokenRepository) RevokeByID(
+	ctx context.Context,
+	id int,
+	revokedAt time.Time,
+) error {
+}
+
+func (r *RefreshTokenRepository) CleanupExpired(
+	ctx context.Context,
+) error {
+}
